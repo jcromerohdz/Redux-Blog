38dd3d87efd4e7024e6a26a5d8eadd0a3e5b5904
J. Christian Romero <jcromerohdz@gmail.com>
2020-04-17 08:57:46 -0700

    Implementing Actions creator for UserHeader component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,18 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = async() => {
-  const response = await jsonPlaceholder.get('/posts'); 
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts');
+
+    dispatch(
+      {
+        type: 'FETCH_POSTS',
+        payload: response.data
+      }
+    )
+};
   
-  return {
-    type: 'FETCH_POST',
-    payload: response
-  };
+export const fetchUser = (id) => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: 'FETCH_USER', payload: response.data });
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
+import postsReducer from './postReducer';
 
 export default combineReducers({
-  replaceMe: () => 'Hi, There!'
+  posts: postsReducer
 });
